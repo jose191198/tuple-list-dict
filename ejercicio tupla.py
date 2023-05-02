c820bcb2e744253ec380d44630de8d796a8a1c1c
jose191198 <83844671+jose191198@users.noreply.github.com>
2023-05-01 23:22:07 -0600

    Add files via upload

---
+++ ejercicio tupla.py
@@ -0,0 +1,17 @@
+
+#Mi ejercicio de tuplas y listas en python
+#Crear un Tuple con diferentes tipos de datos
+ejercicio = ("Lalo", True, False, 10, "Python")
+#Checar tipo de dato
+print(ejercicio)
+print(type(ejercicio))
+
+#Convertir el Tuple en una Lista
+ej_list = list(ejercicio)
+print(type(ej_list))
+print(ej_list)
+
+#Crear un diccionario con los datos de la lista
+ej_dict = {1: "Lalo", 2: "True", 3: "False", 4: "10", 5:"Python"}
+print(type(ej_dict))
+print(ej_dict)
